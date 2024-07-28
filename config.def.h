0ddabcaf404127d473d30320a41bf44e3fe15721
22mahmoud <hello@mahmoudashraf.dev>
2024-07-28 18:31:49 +0300

    add more fonts

--- config.def.h
+++ config.def.h
@@ -6,6 +6,13 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = { 
+  "JoyPixels:pixelsize=10:antialias=true:autohint=true",
+  /* "Noto Sans Arabic:pixelsize=13:antialias=true:autohint=true"  */
+  "Kawkab Mono:pixelsize=13:antialias=true:autohint=true" 
+};
+
 static int borderpx = 2;
 
 /*
